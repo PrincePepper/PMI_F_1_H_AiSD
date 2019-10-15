fa4944fcd3ae86942ec2b893ffb1e1f96173d562
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-10-16 01:55:21 +1000

    2

--- test.c
+++ test.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-#define MAX_ELEMENT 1000
-char mass[MAX_ELEMENT] = {'n'};
-char mass2[MAX_ELEMENT] = {' '};
-
-int main() {
-    gets(mass);
-    int probel = 0;
-    for (int i = 0; i < MAX_ELEMENT ||  mass[i] == 'n'; i++) {
-        if (mass[i] == ' ' || mass[i] == 'n') {
-            probel++;
-        }
-
-    }
-
-    //FILE *fin = fopen("input.txt", "r");
-
-    //fscanf(fin,"%s",)
-
+int main(int argc, char** argv) {
+    FILE *fin = fopen(argv[4], "r");
+    for(int i=0;)
+    return 0;
 }
