968223cddf403cb7834f2d885d54d26b5a5b97f4
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-10-19 21:37:15 +1000

    19.10.2019

--- LiMP/ideas.c
+++ LiMP/ideas.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
+#include <math.h>
+
+double give(double U, double W, double S, double T) {
+    if (U - W == 0) if (S - U * T < 0.00001)return S;
+
+    double t = (S - W * T) / (U - W);
+
+    if (t < 0)t = 0;
+    if (t > T)return -1000000;
+    if ((t * U) > S) return -1000000;
+    return (t * U);
+}
+
+double max3(double a, double b) {
+    if (a > b && fabs(a - b) >= 0.1) {
+        return a;
+    } else return b;
+}
 
 int main() {
     FILE *fin = fopen("input.txt", "r");
     FILE *fout = fopen("output.txt", "w");
-
-    float dlina=0,time=0,u1=0,u2=0,w1=0,w2=0;
-    scanf( "%g%g%g%g%g%g", &dlina,&time,&u1,&u2,&w1,&w2);
-
+    double S = 0, T = 0, u1 = 0, u2 = 0, w1 = 0, w2 = 0;
+    fscanf(fin, "%lf %lf %lf %lf %lf %lf", &S, &T, &u1, &u2, &w1, &w2);
+    double res = max3(give(u1, w2, S, T),
+                      max3(give(u2, w1, S, T),
+                           max3(give(u1, w1, S, T), give(u2, w2, S, T))));
+    fprintf(fout, "%f", res);
 }
-
-
-
-
