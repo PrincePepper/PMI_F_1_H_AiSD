3a0907fa13acc90564378b492ddf96eab42ed9be
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-10-20 00:00:57 +1000

    19.10.2019-3

--- algoritm/progressia.c
+++ algoritm/progressia.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <math.h>
 
 int main() {
     FILE *fin = fopen("input.txt", "r");
@@ -9,18 +8,37 @@ int main() {
     fscanf(fin, "%d", &N);
 
     int mass[N];
+    int mass2[N];
     for (int i = 0; i < N; i++) {
         fscanf(fin, "%d", &mass[i]);
     }
-    int a = 0;
-    for (int i = 0; i < N - 1; i++) {
-        for (int g = 1; g < N; g++) {
-            int b = mass[g] - mass[i];
-            for (int j = g; j < N; j += g) {
-                if (b ==mass[g]-mass[j]){
-                    a++;
-                }else a=0;
+    int a = N / 2;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int v = 0;
+    for (int i = 0; i < N; i++) {
+        for (int g = 1; g <= a; g++) {
+            mass2[c] = mass[i];
+            for (int j = g; j <= (N / 2) * g; j += g) {
+                if (abs(mass[j] - mass[i] - b) == g + 1) {
+                    d = g;
+                    c++;
+                    mass2[c] = j + 1;
+                    b = abs(mass[j] - mass[i]);
+                } else {
+                    d = 0;
+                    break;
+                }
+
             }
+            if (d > v)
+                v = d;
+            b = 0;
+            c = 0;
         }
     }
+    fprintf(fout, "%d\n", (int) round((double) N / (double) v) + 1);
+    for (int i = 0; i <= (N / 2) * v; i += v)
+        fprintf(fout, "%d ", i + 1);
 }
