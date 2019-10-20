645c8e325fb45b8fe4be32fdc6209774d5404cf8
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-10-20 14:22:07 +1000

    20.10.2019-2

---
+++ Long_int/long_int.c
@@ -0,0 +1,59 @@
+/*
+Long integers library implementation
+07.10.2019
+*/
+
+#include <stdlib.h>
+#include "long_int.h"
+
+void init_LongInt(LongInt *a, int val) {
+    int m = a->radix;
+
+    // is the radix positive ?
+    if (m <= 0)
+        exit(1);
+
+    if (val == 0) {
+        a->num_digits = 1;
+        a->digits[0] = 0;
+        return;
+    }
+
+    int i = 0;
+    int b = val;
+    while (b > 0) {
+        a->digits[i++] = b % m;
+        b = b / m;
+    }
+    a->num_digits = i;
+}
+
+void copy_LongInt(LongInt *a, const LongInt *b) {
+    // are the radixes equal ?
+    if (a->radix != b->radix)
+        exit(1);
+
+    a->num_digits = b->num_digits;
+    int i;
+    for (i = 0; i < b->num_digits; i++)
+        a->digits[i] = b->digits[i];
+}
+
+void add_LongInt(LongInt *a, const LongInt *b) {
+    // are the radixes equal ?
+    if (a->radix != b->radix)
+        exit(1);
+
+    // TODO
+
+}
+
+void multiply_LongInt_int(LongInt *a, int c) {
+    if (c == 0) {
+        init_LongInt(a, 0);
+        return;
+    }
+
+    // TODO
+
+}
