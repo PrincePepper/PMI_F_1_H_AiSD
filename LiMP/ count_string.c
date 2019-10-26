c11499409b0c1eb8ca4c33fc4974bd3c10b3e255
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-10-26 22:51:49 +1000

    26.10.2019

--- LiMP/ count_string.c
+++ LiMP/ count_string.c
@@ -2,7 +2,7 @@
 // Created by User on 26.10.2019.
 //
 #include <stdio.h>
-
+#include <string.h>
 #define  MAX_ELEMENT (10000)
 char stroke[MAX_ELEMENT];
 
@@ -10,5 +10,12 @@ int main() {
     FILE *fin = fopen("input.txt", "r");
     FILE *fout = fopen("output.txt", "w");
 
-
+    int count = 0;
+    fgets(stroke, MAX_ELEMENT, fin);
+    char *pstr = strtok(stroke, " @#№!':;()_-+=*&?^%$><~`1234567890");
+    while (pstr != NULL) {
+        pstr = strtok(NULL, " @#№!':;()_-+=*&?^%$><~`1234567890");
+        count++;
+    }
+    fprintf(fout, "Words - %d", count);
 }
