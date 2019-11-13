a3a602dcfc54690319aa970c491750de388f620c
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-11-14 02:49:52 +1000

    14.11.2019-2

--- LiMP/lanterns.c
+++ LiMP/lanterns.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     FILE *fin = fopen("input.txt", "r");
     FILE *fout = fopen("output.txt", "w");
     int mass[100] = {0};
     int kol = 0;
-    int max = 0;
+    int max = 1;
     int value = 0;
     int H = 0, S = 0;
     fscanf(fin, "%d", &kol);
     for (int j = 0; j < kol; j++) {
         fscanf(fin, "%d%d", &S, &H);
-        mass[S - H] += 1;
-        mass[S + H] -= 1;
-        for (int i = 0; i < 100; i++) {
-            value += mass[i];
-            if (value > max)
-                max = value;
+        if (abs(S - H) >= 0) {
+            if (S - H < 0) {
+                mass[0] += 1;
+            } else
+                mass[S - H] += 1;
+        }
+        if (S + H < 100) {
+            mass[S + H] -= 1;
         }
     }
-
+    for (int i = 0; i < 100; i++) {
+        value += mass[i];
+        if (value > max)
+            max = value;
+    }
     fprintf(fout, "%d", max);
 }
--- algoritm/glushchenko_pawns.c
+++ algoritm/glushchenko_pawns.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
-
-//
-// Created by User on 06.11.2019.
-//
-const int N = 8;
-int mass[8][8] = {0};
+#include <math.h>
 
 int main() {
     FILE *fin = fopen("input.txt", "r");
     FILE *fout = fopen("output.txt", "w");
-
-    int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    int x1 = 0, x2 = 0, y1 = 0, y2 = 0, x = 0, y = 0;
     fscanf(fin, "%d%d%d%d", &x1, &y1, &x2, &y2);
-    mass[x1 - 1][y1 - 1] = 1;
-    mass[x2 - 1][y2 - 1] = 1;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
 
+    int x_raz1, x_raz2, y_raz1, y_raz2;
+    int mass[8][8] = {0};
+    for (x = 1; x <= 8; x++) {
+        for (y = 1; y <= 8; y++) {
+            x_raz1 = abs(x - x1);
+            x_raz2 = abs(x - x2);
+            y_raz1 = abs(y - y1);
+            y_raz2 = abs(y - y2);
+
+            mass[x_raz1][y_raz1] += 1;
+            mass[x_raz2][y_raz2] += 1;
         }
     }
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            printf("%d ", mass[i][j]);
+        }
+        printf("\n");
+    }
 }
+//if ((x_raz1 == y_raz1) && (x_raz2 == y_raz2)) {
+//                if (((x > x1) && (x2 > x)) || ((y > y1) && (y2 > y)) || ((x1 > x) && (x > x2)) ||
+//                    ((y1 > y) && (y > y2))) {
+//                    fprintf(fout, "%i %i", x, y);
+//                    return 0;
+//                }
+//            }
