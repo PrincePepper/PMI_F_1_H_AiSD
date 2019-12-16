82850e58498ad6d0e0c782175fa72707dd97e05d
Семен <43268369+PrincePepper@users.noreply.github.com>
2019-12-17 01:14:37 +1000

    17.12.2019-03

--- algoritm/guess_word.c
+++ algoritm/guess_word.c
@@ -15,14 +15,14 @@ int main() {
     fscanf(fin, "%s %s", word, combination);
 
     int len_word = strlen(word);
+    int len_comb = strlen(combination);
 
     int mass[len_word];
     for (int i = 0; i < len_word; i++) {
         mass[i] = 0;
     }
+
     int alphabet_new[26] = {0};
-
-
     for (int j = 0; j <= 20; j++) {
         for (int i = 'a'; i < 'z'; i++)
             if (combination[j] == i) {
@@ -32,7 +32,7 @@ int main() {
 
     char alphabet2[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
                           't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    for (int j = 0; j < 16; j++) {
+    for (int j = 0; j < len_word; j++) {
         for (int i = 0; i < 26; i++) {
             if (alphabet2[i] == word[j]) {
                 if (alphabet_new[i] % 2 == 0) {
