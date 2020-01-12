c7188e214825d04a6f43e5964b459c2a855eb387
Семен <43268369+PrincePepper@users.noreply.github.com>
2020-01-12 15:08:37 +1000

    12.01.2020

--- LiMP/ finding_vasya.c
+++ LiMP/ finding_vasya.c
@@ -1,52 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int compare(const void *x1, const void *x2) {
-    return (*(int *) x2 - *(int *) x1);
-}
-
 int main() {
     FILE *fin = fopen("input.txt", "r");
     FILE *fout = fopen("output.txt", "w");
 
-    int N = 0, M = 0, X = 0, K = 0;
+    int N, M, X, K;
     fscanf(fin, "%d%d%d%d", &N, &M, &X, &K);
 
-    int mass[K];
-    int temporary = 0;
+    int A, podiezd, floor;
+    int *house = (int *) calloc(N + 1, sizeof(int));
     for (int i = 0; i < K; i++) {
-        fscanf(fin, "%d", &mass[i]);
-        if (mass[i] * N * M > X)temporary = 1;
-    }
-    if (temporary == 1) {
-        qsort(mass, K, sizeof(int), compare);
-        int mass_etag[N];
-        mass_etag[0] = 0;
-        int count = 0;
-        int podiez = 0;
-        int v = 0, v2 = 0;
-        int count2 = 0;
-        for (int i = 0; i < K; i++) {
-            for (int g = 1; g <= N; g++) {
-                v = g;
-                int a = mass[i] * M * g;
-                mass_etag[g - 1] = a;
-                podiez = mass_etag[g - 1] - mass[i] * M;
-                if (mass_etag[g - 1] >= X) break;
-            }
-            if (v2 == v) {
-                podiez = count2 * mass[i];
-            } else count2 = 0;
+        fscanf(fin, "%d", &A);
+        podiezd = (X - 1) / (M * A) + 1;
+        floor = ((X - (podiezd - 1) * M * A) - 1) / A + 1;
 
-            while (podiez < X) {
-
-                podiez += mass[i];
-                count++;
-            }
-            count2 = count;
-            v2 = v;
+        if ((podiezd - 1) <= N && (floor > house[podiezd])) {
+            house[podiezd] = floor;
         }
-        fprintf(fout, "%d", count);
-    } else fprintf(fout, "-1");
+    }
+    int sum = 0;
+    for (int i = 0; i < N + 1; i++) {
+        sum += house[i];
+    }
+    if (sum == 0) fprintf(fout, "%s", "-1");
+    else fprintf(fout, "%d", sum);
+
 }
